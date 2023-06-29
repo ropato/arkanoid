187c2aaa888b28140b8794dc7afc0f43e7f96766
Agustin <agus.lebed@gmail.com>
2023-06-29 17:44:59 -0300

    PowerUP masFuerza

---
+++ pelota.py
@@ -0,0 +1,27 @@
+import pygame
+import threading
+
+class Pelota(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        super().__init__()
+        self.image = pygame.image.load("pelota.png").convert()
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.velocidadH = 5
+        self.velocidadV = 5
+        self.fuerza = 1
+
+    def update(self,limX,limY,choquevertical = False, choqueHorizontal = False):
+        
+        if self.rect.right >= limX or self.rect.left <= 0 or choqueHorizontal:
+            self.velocidadH *= -1
+        if self.rect.top <= 0 or choquevertical:
+            self.velocidadV *= -1
+
+        self.rect.x += self.velocidadH
+        self.rect.y += self.velocidadV
+        
+    def masFuerza(self):
+        self.fuerza = 100
+
