98315b38a2ec2629066bbd0a2d353a1994c29936
Agustin <agus.lebed@gmail.com>
2023-07-01 13:00:53 -0300

    Power up de achicar

--- jugador.py
+++ jugador.py
@@ -1,13 +1,16 @@
 import pygame
 
 Scale_Factor = 1.2
+
 class Jugador(pygame.sprite.Sprite):
+
     def __init__(self,x,y):
         super().__init__()
         self.image = pygame.image.load("jugador.png")
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.ANCHO_INICIAL = self.rect.width
         
     
     def izquierda(self,limX):
@@ -25,7 +28,15 @@ class Jugador(pygame.sprite.Sprite):
             self.rect.x += 10
 
     def getBig(self):
-        tempX = self.rect.x
-        tempY = self.rect.y
-        self.image = pygame.transform.scale(self.image, (self.rect.width * Scale_Factor, self.rect.height))
-        self.rect = self.image.get_rect(x=tempX, y=tempY)
+        if not self.rect.width >= self.ANCHO_INICIAL * 2:
+            tempX = self.rect.x
+            tempY = self.rect.y
+            self.image = pygame.transform.scale(self.image, (self.rect.width * Scale_Factor, self.rect.height))
+            self.rect = self.image.get_rect(x=tempX, y=tempY)
+
+    def getSmall(self):
+        if not self.rect.width<= self.ANCHO_INICIAL / 2:
+            tempX = self.rect.x
+            tempY = self.rect.y
+            self.image = pygame.transform.scale(self.image, (self.rect.width / Scale_Factor, self.rect.height))
+            self.rect = self.image.get_rect(x=tempX, y=tempY)
