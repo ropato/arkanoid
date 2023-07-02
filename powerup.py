eabd3831678953fcd5ba4f8c8713f795042e69cb
Javierppt <arielriverappt@gmail.com>
2023-07-02 18:34:14 -0300

    refactorizacion de codigo, ahora todas las variables estan en ingles mejore/arregle todas las funciones que escriben en pantalla evite que el brickGroup se re-dibuje cada vez que haya un tick las colisiones con brickGroup ahora funciona para mas de un brick

--- powerup.py
+++ powerup.py
@@ -1,17 +1,15 @@
 import pygame
 SPRITE_SIZE = (50, 50)
 class powerUp (pygame.sprite.Sprite):
-    def __init__(self,x,y, powerUp,imagen:str):
+    def __init__(self,x,y, powerUp,image:str):
         super().__init__()
-        self.image = pygame.image.load(imagen)
+        self.image = pygame.image.load(image)
         self.image = pygame.transform.scale(self.image,SPRITE_SIZE)
         self.rect=self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
         self.powerUp = powerUp
-        self.velocidadV = 3
+        self.verticalSpeed = 3
 
-
-   
     def fallDown(self):
-        self.rect.y+= self.velocidadV
+        self.rect.y+= self.verticalSpeed
